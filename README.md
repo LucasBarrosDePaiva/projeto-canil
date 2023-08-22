e5880b4c4af7e02d7b96cf9c3a07ddd71e3ea19a
lucasbarros <lucasbarros12@hotmail.com>
2023-08-22 11:08:54 -0300

    Configuring node server

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # projeto-canil
-Pet adoption project.
+
+Pet adoption project, made with Node + Typescript.
+
+### global requirements
+
+    `npm i -g nodemon typescript ts-node`
+
+### installation
+
+    `npm install`
+
+### run the project
+
+    `npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import mustache from "mustache-express";
+import path from "path";
+import mainRoutes from "./routes/index";
+
+dotenv.config();
+
+const server = express();
+
+server.set("view engine", "mustache"); //configure mustache
+server.set("views", path.join(__dirname, "views")); //create views in rootDir src
+server.engine("mustache", mustache()); //run mustache
+
+server.use(express.static(path.join(__dirname, "../public")));
+
+server.use(mainRoutes); //rotes
+server.use((req, res) => {
+    res.send("Page not found!");
+});
+
+server.listen(process.env.PORT);
